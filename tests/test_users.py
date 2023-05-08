5f82a06d3a10f24fb9cbaa825d65a4aa05aef843
Kanishk <kanishksharma809@gmail.com>
2023-05-08 23:52:56 +0530

    finished alembic, docker, started pytests

---
+++ tests/test_users.py
@@ -0,0 +1,9 @@
+from fastapi.testclient import TestClient
+from app.main import app
+
+client = TestClient(app)
+
+def test_root():
+    res = client.get("/")
+    assert res.json().get('message')=='Hello World...!'
+    assert res.status_code==200
